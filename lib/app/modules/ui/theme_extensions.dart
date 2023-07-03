33e442de5edd91cf76ab8d4e5691db528aad8f7f
dionatanlaranjeira <dionatanribeiro.netrr@gmail.com>
2023-07-03 17:59:49 -0300

    login page

---
+++ lib/app/modules/ui/theme_extensions.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+extension ThemeExtensions on BuildContext {
+  Color get primaryColor => Theme.of(this).primaryColor;
+  Color get primaryColorLight => Theme.of(this).primaryColorLight;
+  // Color get buttonColor => Theme.of(this).buttonColor;
+  TextTheme get textTheme => Theme.of(this).textTheme;
+  }
