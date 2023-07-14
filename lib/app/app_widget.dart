31515d8982a77e2d4732bc2b5f6320398cfcfae5
dionatanlaranjeira <dionatanribeiro.netrr@gmail.com>
2023-07-14 18:32:43 -0300

    login page

--- lib/app/app_widget.dart
+++ lib/app/app_widget.dart
@@ -1,10 +1,8 @@
 import 'package:flutter/material.dart';
-// import 'package:provider/provider.dart';
 import 'package:todo_list_provider/app/core/database/migrations/sqlite_adm_connection.dart';
-import 'package:todo_list_provider/app/modules/auth/login/login_page.dart';
+import 'package:todo_list_provider/app/core/ui/todo_list_ui_config.dart';
+import 'package:todo_list_provider/app/modules/auth/auth_module.dart';
 import 'package:todo_list_provider/app/modules/splash/splash_page.dart';
-// import '../app/modules/auth/login/login_page.dart';
-// import 'modules/auth/login/login_controller.dart';
 
 class AppWidget extends StatefulWidget {
   const AppWidget({Key? key}) : super(key: key);
@@ -29,28 +27,12 @@ class _AppWidgetState extends State<AppWidget> {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       title: 'Todo List Provider',
-      // initialRoute: '/login',
-      // routes: {
-        // '/login': (_) => MultiProvider(
-        //       providers: [
-        //         Provider(create: (_) => 'Repository'),
-        //         Provider(create: (_) => 'Service'),
-        //         ChangeNotifierProvider(create: (_) => LoginController()),
-        //       ],
-        //       child: const LoginPage(),
-        //     ),
-        // '/register': (_) => MultiProvider(
-        //       providers: [
-        //         Provider(create: (_) => 'Repository'),
-        //         Provider(create: (_) => 'Service'),
-        //         ChangeNotifierProvider(create: (_) => LoginController()),
-        //       ],
-        //       child: const LoginPage(),
-        //     ),
-      // },
-      home: LoginPage(),
+      initialRoute: '/login',
+      theme: TodoListUiConfig.theme,
+      routes: {...AuthModule().routers},
+      home: const SplashPage(),
     );
   }
 }
--- lib/app/modules/auth/login/login_page.dart
+++ lib/app/modules/auth/login/login_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:sign_in_button/sign_in_button.dart';
 import 'package:todo_list_provider/app/widgets/todo_list_logo.dart';
 
 class LoginPage extends StatelessWidget {
@@ -62,6 +63,39 @@ class LoginPage extends StatelessWidget {
                         ),
                       ),
                     ),
+                    const SizedBox(height: 20),
+                    Expanded(
+                        child: Container(
+                      decoration: BoxDecoration(
+                        color: const Color(0XFFF0F3F7),
+                        border: Border(
+                          top: BorderSide(
+                            width: 2,
+                            color: Colors.grey.withAlpha(50),
+                          ),
+                        ),
+                      ),
+                      child: Column(
+                        children: [
+                          const SizedBox(
+                            height: 30,
+                          ),
+                          SignInButton(
+                            Buttons.google,
+                            text: 'Continue com o Google',
+                            padding: const EdgeInsets.all(5),
+                            shape: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(30),
+                                borderSide: BorderSide.none),
+                            onPressed: () {},
+                          ),
+                          Row(mainAxisAlignment: MainAxisAlignment.center, children: [
+                            const Text('Não tem conta?'),
+                            TextButton(onPressed: (){}, child: const Text('Registre-se'))
+                          ],)
+                        ],
+                      ),
+                    ))
                   ],
                 ),
               ),
